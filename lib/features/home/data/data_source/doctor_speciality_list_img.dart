36a012c611619b400e2e8ca78d2a7a32cd53ade6
Moustafa AbdElhady <Moustafa-Abdelhady>
2024-08-20 15:55:01 +0300

    added speciality list img

---
+++ lib/features/home/data/data_source/doctor_speciality_list_img.dart
@@ -0,0 +1,5 @@
+import 'package:reservations_app/features/home/data/models/static_data_source/doctor_speciality_img_item.dart';
+
+List<DoctorSpecialityImgItem> specialityListImg =[
+  DoctorSpecialityImgItem(img: '')
+]
