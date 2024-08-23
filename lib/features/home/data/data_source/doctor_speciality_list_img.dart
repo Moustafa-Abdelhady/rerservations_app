f612149adf8cd2c93fb3227c7791f0265071147e
Moustafa AbdElhady <Moustafa-Abdelhady>
2024-08-24 02:23:10 +0300

    added speciality list image as static in home page

--- lib/features/home/data/data_source/doctor_speciality_list_img.dart
+++ lib/features/home/data/data_source/doctor_speciality_list_img.dart
@@ -1,5 +1,14 @@
 import 'package:reservations_app/features/home/data/models/static_data_source/doctor_speciality_img_item.dart';
 
-List<DoctorSpecialityImgItem> specialityListImg =[
-  DoctorSpecialityImgItem(img: '')
-]
+List<DoctorSpecialityImgItem> doctorSpecialityListImg = [
+  DoctorSpecialityImgItem(img: 'assets/images/Cardiology.png'),
+  DoctorSpecialityImgItem(img: 'assets/images/Dermatology.png'),
+  DoctorSpecialityImgItem(img: 'assets/images/Neurologic.png'),
+  DoctorSpecialityImgItem(img: 'assets/images/Orthopedics.png'),
+  DoctorSpecialityImgItem(img: 'assets/images/Pediatric.png'),
+  DoctorSpecialityImgItem(img: 'assets/images/Gynecology.png'),
+  DoctorSpecialityImgItem(img: 'assets/images/Optometry.png'),
+  DoctorSpecialityImgItem(img: 'assets/images/Urologist.png'),
+  DoctorSpecialityImgItem(img: 'assets/images/Gastroenterology.png'),
+  DoctorSpecialityImgItem(img: 'assets/images/Psychiatry.png'),
+];
