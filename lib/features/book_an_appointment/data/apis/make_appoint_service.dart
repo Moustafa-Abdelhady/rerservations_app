9a54498b75e7a2986eebdaebde60599336e4fc89
Moustafa AbdElhady <Moustafa-Abdelhady>
2024-09-18 18:39:27 +0300

    make a new branch makeAppoint-logic

--- lib/features/book_an_appointment/data/apis/make_appoint_service.dart
+++ lib/features/book_an_appointment/data/apis/make_appoint_service.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:reservations_app/core/networking/api_constans.dart';
 import 'package:reservations_app/features/book_an_appointment/data/apis/make_appoint_apis.dart';
 import 'package:reservations_app/features/book_an_appointment/data/models/make_appoint_request_body.dart';
+import 'package:retrofit/error_logger.dart';
 import 'package:retrofit/http.dart';
 
 part 'make_appoint_service.g.dart';
